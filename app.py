a5092a354a2e4c0c633e6f5f3cb7a5a09507511b
root <roman_kiper@mail.ru>
2024-03-10 22:05:55 +0300

    10/03-2

--- app.py
+++ app.py
@@ -1,28 +1,54 @@
 import asyncio
+import logging
 import os
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
 
 from dotenv import find_dotenv, load_dotenv
+
 load_dotenv(find_dotenv())
+
+from database.engine import create_db, drop_db, session_maker
 
 from handlers.user_private import user_private_router
 from handlers.user_group import user_group_router
 from keyboards.main_menu import set_main_menu
+from middlewares.db import DataBaseSession
 
 ALLOWED_UPDATES = ['message, edited_message']
 
+logger = logging.getLogger(__name__)
 
-bot = Bot(token=os.getenv('TOKEN'), parse_mode=ParseMode.HTML)
+bot = Bot(token=os.getenv('TOKEN'), parse_mode="HTML")
 dp = Dispatcher()
 
 dp.include_router(user_private_router)
 dp.include_router(user_group_router)
 
 
+async def on_startup(bot):
+
+    run_param = False
+    if run_param:
+        await drop_db()
+    await create_db()
+
+async def on_shutdown(bot):
+    print('Бот лег!')
+
+
 async def main():
+    logging.basicConfig(level=logging.INFO,
+                        format='%(filename)s:%(lineno)d #%(levelname)-8s '               
+                               '[%(asctime)s] - %(name)s - %(message)s')
+    logger.info('Starting bot')
+
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
+
+    dp.update.middleware(DataBaseSession(session_pool=session_maker))
+    await bot.send_message(id=ID_MAIN_ADMIN, text='Бот запущен!')
     await set_main_menu(bot)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
---
+++ database/models.py
@@ -0,0 +1,28 @@
+from sqlalchemy import String, Float, Text, DateTime, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    created: Mapped[DateTime] = mapped_column(DateTime, default=func.now())
+    updated: Mapped[DateTime] = mapped_column(DateTime, default=func.now(), onupdate=func.now())
+
+class Advertising(Base):
+    __tablename__ = 'advertising'
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(150), nullable=False)
+    description: Mapped[str] = mapped_column(Text)
+    price: Mapped[float] = mapped_column(Float(asdecimal=True), nullable=False)
+    image: Mapped[str] = mapped_column(String(150))
+
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    # Телеграм user_id
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    username: Mapped[str] = mapped_column(String(64), nullable=True, unique=True)
+
+    def __str__(self) -> str:
+        return f"<User:{self.id}>"
