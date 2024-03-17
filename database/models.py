eafa36e07da01e98fac22cf82ec5d6777c936ebb
root <roman_kiper@mail.ru>
2024-03-18 00:52:58 +0300

    17/03-1

--- database/models.py
+++ database/models.py
@@ -1,10 +1,28 @@
-from sqlalchemy import String, Float, Text, DateTime, func
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String, Float, Text, DateTime, func, ForeignKey, Numeric
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
 class Base(DeclarativeBase):
     created: Mapped[DateTime] = mapped_column(DateTime, default=func.now())
     updated: Mapped[DateTime] = mapped_column(DateTime, default=func.now(), onupdate=func.now())
+
+
+class Banner(Base):
+    __tablename__ = 'banner'
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(64), unique=True)
+    description: Mapped[str] = mapped_column(Text, nullable=True)
+    image: Mapped[str] = mapped_column(String(150), nullable=True)
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(150), nullable=False)
+
+
 
 class Product(Base):
     __tablename__ = 'product'
@@ -12,17 +30,42 @@ class Product(Base):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(150), nullable=False)
     description: Mapped[str] = mapped_column(Text)
-    price: Mapped[float] = mapped_column(Float(asdecimal=True), nullable=False)
+    price: Mapped[float] = mapped_column(Numeric(6,2), nullable=False)
     image: Mapped[str] = mapped_column(String(150))
+    category_id: Mapped[int] = mapped_column(ForeignKey('category.id', ondelete='CASCADE'), nullable=False)
+
+    category: Mapped['Category'] = relationship(backref='product')
 
 
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "user"
 
     # Телеграм user_id
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    username: Mapped[str] = mapped_column(String(64), nullable=True, unique=True)
+    user_id: Mapped[int] = mapped_column(unique=True)
+    first_name: Mapped[str] = mapped_column(String(150), nullable=True)
+    last_name: Mapped[str] = mapped_column(String(150), nullable=True)
+    phone: Mapped[str] = mapped_column(String(13), nullable=True)
 
-    def __str__(self) -> str:
-        return f"<User:{self.id}>"
+
+# Корзина, т.е сущность объединяющая предложения, товары.
+class Cart(Base):
+    __tablename__ = 'cart'
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.user_id', ondelete='CASCADE'), nullable=False)
+    product_id: Mapped[int] = mapped_column(ForeignKey('product.id', ondelete='CASCADE'), nullable=False)
+    quantity: Mapped[int]
+    discount: Mapped[int] = mapped_column(default='0')
+
+    user: Mapped['User'] = relationship(backref='cart')
+    product: Mapped['Product'] = relationship(backref='cart')
+
+
+
+
+
+
+
+
