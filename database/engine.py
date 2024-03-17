8df0bc68e74afaf9d996fa04848641a66c6ad95a
root <roman_kiper@mail.ru>
2024-03-17 22:25:38 +0300

    15/03-2

--- database/engine.py
+++ database/engine.py
@@ -7,6 +7,12 @@ from database.models import Base
 # DB_LITE=sqlite+aiosqlite:///my_base.db   код из файла .env
 # engine = create_async_engine(os.getenv('DB_LITE'), echo=True)
 
+# BOT_TOKEN=7158259959:AAFgv0H3zVymsKexx87B0JPvixBG3zAu4S0
+# DB_URL=postgresql+asyncpg://dev:golovchenko84@localhost:5432/bun_presentation
+# ID_MAIN_ADMIN=1006569664
+# ID_ADMINS=1006569664
+
+
 engine = create_async_engine(os.getenv('DB_URL'), echo=True)
 
 session_maker = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
